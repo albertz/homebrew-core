d441b55a6bebca873921daf0a06cbd262526c5e6
Albert Zeyer <albert.zeyer@rwth-aachen.de>
2021-12-01 20:34:56 +0100

    texmaker formula

---
+++ Formula/texmaker.rb
@@ -0,0 +1,54 @@
+class Texmaker < Formula
+  desc "Free cross-platform LaTeX editor since 2003"
+  homepage "https://www.xm1math.net/texmaker/"
+  url "https://www.xm1math.net/texmaker/texmaker-5.1.2.tar.bz2"
+  sha256 "526896f2c1ae561130eec7aae815b9dcda9e8eeb772b6541d0dc94ce91a71044"
+  license "GPL-2.0"
+
+  depends_on "qt"
+
+  on_linux do
+    depends_on "gcc"
+  end
+
+  fails_with gcc: "5"
+
+  patch :p0, :DATA
+
+  def install
+    args = [
+      "PREFIX=#{prefix}",
+      "DESKTOPDIR=#{prefix}/share/applications",
+      "ICONDIR=#{prefix}/share/pixmaps",
+      "METAINFODIR=#{prefix}/share/metainfo",
+      "QMAKE_CXXFLAGS=#{ENV.cxxflags}",
+      "-config", "release", "-spec"]
+    os = OS.mac? ? "macx" : OS.kernel_name.downcase
+    compiler = ENV.compiler.to_s.start_with?("gcc") ? "g++" : ENV.compiler
+    arch = Hardware::CPU.intel? ? "" : "-#{Hardware::CPU.arch}"
+    args << "#{os}-#{compiler}#{arch}"
+
+    system Formula["qt"].opt_bin/"qmake", *args
+    system "make"
+    system "make", "install"
+  end
+end
+
+__END__
+--- icondelegate.cpp.old	2021-12-01 18:56:51.990313349 +0000
++++ icondelegate.cpp	2021-12-01 19:06:14.410557704 +0000
+@@ -335,9 +335,11 @@
+     //QString key;
+     //key.sprintf("%d-%d", pixmap.serialNumber(), enabled);
+     QString key = qPixmapSerial(pixmap.cacheKey(), enabled);
+-    QPixmap *pm = QPixmapCache::find(key);
+-//    QPixmap *pm;
+-//    QPixmapCache::find(key,pm);
++    QPixmap *pm = new QPixmap();
++    if(QPixmapCache::find(key, pm))
++        return pm;
++    delete pm;
++    pm = NULL;
+     if (!pm) {
+         QImage img = pixmap.toImage().convertToFormat(QImage::Format_ARGB32_Premultiplied);
+ 
